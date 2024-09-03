b29521de951268a6617990de3411e667dcde288a
NikolaBjelica <nbjelica507@gmail.com>
2024-09-03 09:50:43 -0400

    Updated Dual Motor controls program

--- dual-motor.c
+++ dual-motor.c
@@ -24,30 +24,30 @@ void setup()
   // Setup for Motor 1
   pinMode(STEP_1, OUTPUT);
   pinMode(DIR_1, OUTPUT);
-  digitalWrite(ENABLE_1, LOW);
+  // digitalWrite(ENABLE_1, LOW);
   digitalWrite(SLEEP_1, LOW);
   digitalWrite(RESET_1, LOW);
-  digitalWrite(MS1_1, LOW);
-  digitalWrite(MS2_1, LOW);
-  digitalWrite(MS3_1, LOW);
+  // digitalWrite(MS1_1, LOW);
+  // digitalWrite(MS2_1, LOW);
+  // digitalWrite(MS3_1, LOW);
 
   // Setup for Motor 2
   pinMode(STEP_2, OUTPUT);
   pinMode(DIR_2, OUTPUT);
-  digitalWrite(ENABLE_2, HIGH);
+  // digitalWrite(ENABLE_2, HIGH);
   digitalWrite(SLEEP_2, LOW);
   digitalWrite(RESET_2, LOW);
-  digitalWrite(MS1_2, LOW);
-  digitalWrite(MS2_2, LOW);
-  digitalWrite(MS3_2, LOW);
+  // digitalWrite(MS1_2, LOW);
+  // digitalWrite(MS2_2, LOW);
+  // digitalWrite(MS3_2, LOW);
 }
 
 void loop()
 {
   digitalWrite(DIR_1, HIGH);
-  Serial.println("Spinning Clockwise...");
+  Serial.println("Spinning Motor 1...");
 
-  for (int i = 0; i < 200; i++)
+  for (int i = 0; i < 500; i++)
   {
     digitalWrite(STEP_1, HIGH);
     delayMicroseconds(1000);
@@ -56,9 +56,9 @@ void loop()
   }
 
   digitalWrite(DIR_2, HIGH);
-  Serial.println("Spinning Clockwise...");
+  Serial.println("Spinning Motor 2...");
 
-  for (int i = 0; i < 200; i++)
+  for (int i = 0; i < 500; i++)
   {
     digitalWrite(STEP_2, HIGH);
     delayMicroseconds(1000);
